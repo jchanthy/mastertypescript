ff7c040b05df4a19fde71d1bb20e899cc5fe7854
JOHN Chanthy <john.chanthy@gmail.com>
2021-08-17 14:38:17 +0700

    I added condition_expression file

---
+++ CH01/condition_expression.js
@@ -0,0 +1,3 @@
+var value = 10;
+var message = value > 10 ? "value is larger then 10" : "value is 10 or less";
+console.log(message);
---
+++ CH01/condition_expression.ts
@@ -0,0 +1,5 @@
+const value:number = 10;
+const message:string = value > 10 ? "value is larger then 10" : "value is 10 or less" ;
+
+console.log(message);
+
